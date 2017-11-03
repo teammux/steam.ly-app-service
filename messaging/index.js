5eedc6ed46aa9318adbd6330f228ecd877ecd7c6
Lam Bui <lamdbui@gmail.com>
2017-11-03 11:33:23 -0700

    Add sendUser and sendEvent methods for SQS communication

--- messaging/index.js
+++ messaging/index.js
@@ -1,7 +1,10 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+
 const AWS = require('aws-sdk');
-const { MessageConfig } = require('./config.js');
 const db = require('../db/index.js');
-db.open();
+
+// TODO: maybe move this into a config
+const SERVICE_NAME = 'app-service';
 
 // NOTE: These envvars must be set for Amazon SQS services to work
 // AWS_ACCESS_KEY_ID
@@ -12,145 +15,96 @@ db.open();
 //   secretAccessKey: process.key.AWS_SECRET_ACCESS_KEY,
 // });
 
+// do AWS setup
+// TODO: maybe move this to a config
 AWS.config.update({ region: 'us-west-1' });
-
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
-const dbOpen = () => {
-  if (!db.get()) {
-    db.open();
-  }
-};
-
-const dbClose = () => {
-  db.close();
-};
-
-// TODO: Finish making changes here and maybe change to allow passing in attributes object as well
-const sendMessage = (url, type, body) => {
-  // TODO: testing just for now...
-  const params = {
-    MessageAttributes: {
-      Type: {
-        DataType: 'String',
-        StringValue: type,
-      },
-      Date: {
-        DataType: 'String',
-        StringValue: Date.now().toString(),
-      },
-      UserId: {
-        DataType: 'String',
-        StringValue: 'user_12',
-      },
-    },
-    MessageBody: JSON.stringify(body),
-    QueueUrl: url,
-  };
-  return new Promise((resolve, reject) => {
-    sqs.sendMessage(params, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data.MessageId);
-      }
-    });
-  });
-};
-
-const receiveMessage = (url) => {
-  const receiveParams = {
-    AttributeNames: [
-      'SentTimestamp',
-    ],
-    MaxNumberOfMessages: 1,
-    MessageAttributeNames: [
-      'All',
-    ],
-    QueueUrl: url,
-    VisibilityTimeout: 0,
-    WaitTimeSeconds: 0,
-  };
-
-  return new Promise((resolve, reject) => {
-    sqs.receiveMessage(receiveParams, (err, data) => {
-      if (err) {
-        // console.log('Receive Error', err);
-        reject(err);
-      } else {
-        // console.log('Received:', data);
-        resolve(data);
-        // delete it too!
-        const deleteParams = {
-          QueueUrl: url,
-          ReceiptHandle: data.Messages[0].ReceiptHandle,
-        };
-        sqs.deleteMessage(deleteParams, (deleteErr, deleteData) => {
-          if (deleteErr) {
-            console.log('Delete Error', deleteErr);
-            // reject(deleteErr);
-          } else {
-            console.log('Message Deleted', deleteData);
-            // resolve(deleteData);
-          }
-        });
-      }
-    });
-  });
-};
-
-const sendUser = (url, userId) => {
-  // db.open(url)
-  //   .then(() => {
-  //     db.getUserById(userId)
-  //       .then((data) => {
-  //         console.log('*** MY DATA:', data);
-  //         // sendMessage(url, 'user', data);
-  //         db.close();
-  //       })
-  //       .catch(() => {
-  //         db.close();
-  //       });
-  //   })
-  //   .catch(() => {
-  //     // connection error
-  //   });
-  // db.open()
-  //   .then(() => {
-  //     db.getUserById(userId)
-  //       .then((data) => {
-  //         console.log('*** MY DATA:', data);
-  //         // sendMessage(url, 'user', data);
-  //         dbClose();
-  //       })
-  //       .catch(() => {
-  //         db.close();
-  //       });
-  //   })
-  //   .catch(() => {
-  //
-  //   });
-  // db.open()
-  //   .then(() => {
-  //
-  //   })
+const sendUser = (url, userId, isFifo = false) => {
   // fetch from db
   db.getUserById(userId)
     .then((data) => {
-      console.log('*** MY DATA:', data);
-      // sendMessage(url, 'user', data);
+      const userBody = {
+        id: data._id,
+        username: data.username,
+        preference: data.preference,
+        location: data.location,
+        age: data.age,
+        gender: data.gender,
+      };
+
+      const params = {
+        MessageAttributes: {
+          Type: {
+            DataType: 'String',
+            StringValue: 'user',
+          },
+          Date: {
+            DataType: 'String',
+            StringValue: Date.now().toString(),
+          },
+          UserId: {
+            DataType: 'String',
+            StringValue: userId.toString(),
+          },
+        },
+        MessageBody: JSON.stringify(userBody),
+        QueueUrl: url,
+      };
+
+      // additional params for FIFO queues
+      if (isFifo) {
+        params.MessageGroupId = SERVICE_NAME;
+        params.MessageDeduplicationId = SERVICE_NAME;
+      }
+
+      sqs.sendMessage(params, (err, messageData) => {
+        if (err) {
+          console.log('error sending message:', err);
+        } else {
+          console.log('send message:', messageData.MessageId);
+        }
+      });
     })
     .catch(() => {
     });
   // send to message url
 };
 
-const sendEvent = (url, eventId) => {
-  // fetch event
-  // send to message url
+const sendEvent = (url, eventId, messageBody = {}, isFifo = false) => {
+  const params = {
+    MessageAttributes: {
+      Type: {
+        DataType: 'String',
+        StringValue: 'user',
+      },
+      Date: {
+        DataType: 'String',
+        StringValue: Date.now().toString(),
+      },
+      EventId: {
+        DataType: 'String',
+        StringValue: eventId.toString(),
+      },
+    },
+    MessageBody: JSON.stringify(messageBody),
+    QueueUrl: url,
+  };
+
+  // additional params for FIFO queues
+  if (isFifo) {
+    params.MessageGroupId = SERVICE_NAME;
+    params.MessageDeduplicationId = SERVICE_NAME;
+  }
+
+  sqs.sendMessage(params, (err, messageData) => {
+    if (err) {
+      console.log('error sending message:', err);
+    } else {
+      console.log('send message:', messageData.MessageId);
+    }
+  });
 };
 
-module.exports.dbOpen = dbOpen;
-module.exports.dbClose = dbClose;
 module.exports.sendUser = sendUser;
 module.exports.sendEvent = sendEvent;
--- test/testMessaging.js
+++ test/testMessaging.js
@@ -1,20 +1,13 @@
-/* global describe, before, after, it */
+/* global describe, it */
+
 const assert = require('assert');
 const messaging = require('../messaging/index.js');
 
 describe('steam.ly - messaging', () => {
-  before(() => {
-    messaging.dbOpen();
-  });
-
   describe('API', () => {
     it('"sendUser and "sendEvent" should exist', () => {
       assert(messaging.sendUser);
       assert(messaging.sendEvent);
     });
-  });
-
-  after(() => {
-    messaging.dbClose();
   });
 });
